e9e2823efe4794bcf7332e2ec3a4ed29422d2a81
SnowBlueChain <frankwong661@gmail.com>
2018-03-20 22:19:44 -0400

    Moved api route location to app dir

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Angular pref DIST output folder
 app.use(express.static(path.join(_dirname, 'dist')));
 
-// API Location
-app.use('/api', api);
-
 // Send all other requests to the ng app
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
